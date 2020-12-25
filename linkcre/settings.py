7893801599cd755530f2a90af4d90b3a7714759f
tanajim <tanajim580@gmail.com>
2020-12-25 18:38:40 +0530

    firts commit

--- linkcre/settings.py
+++ linkcre/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'knbf^g6_p&t6b04p-oy#k^743v#_%rt@3*@@4m@chvw45t*yps'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','tubeloads.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','tubeloads.herokuapp.com/']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-    
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
