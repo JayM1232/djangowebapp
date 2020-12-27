b2c754f9d0c7c5aa109b216a23330790234ee8cc
tanajim <tanajim580@gmail.com>
2020-12-27 13:14:51 +0530

    changes done to profile

--- linkcre/settings.py
+++ linkcre/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-# import django_heroku
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,4 +128,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
