c53a0bdbce490d9f1e6e1d083900b0886f5ad73e
tanajim <tanajim580@gmail.com>
2020-12-28 11:15:01 +0530

    changes done

--- linkcre/settings.py
+++ linkcre/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 import os
 # import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -26,7 +26,7 @@ SECRET_KEY = 'knbf^g6_p&t6b04p-oy#k^743v#_%rt@3*@@4m@chvw45t*yps'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tubeloads.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','tubeloads.herokuapp.com']
 # ALLOWED_HOSTS = []
 
 # Application definition
@@ -119,21 +119,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
-
-# STATICFILES_DIRS=[
-#     os.path.join(BASE_DIR,'static')
-# ]
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static'),
+]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 # django_heroku.settings(locals())
