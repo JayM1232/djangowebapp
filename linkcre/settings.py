6df94e1f4fdaa1bcee0414cb2c5c38c208a5d4b6
tanajim <tanajim580@gmail.com>
2020-12-28 11:21:00 +0530

    changes done

--- linkcre/settings.py
+++ linkcre/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Quick-start development settings - unsuitable for production
