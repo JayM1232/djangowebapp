6594d533c37c2601018b08708cbc17d206365c58
tanajim <tanajim580@gmail.com>
2020-12-28 11:40:33 +0530

    changes done

--- linkcre/settings.py
+++ linkcre/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from django.conf.global_settings import STATIC_ROOT,STATICFILES_STORAGE,STATICFILES_DIRS,STATIC_URL
+# from django.conf.global_settings import STATIC_ROOT,STATICFILES_STORAGE,STATICFILES_DIRS,STATIC_URL
 # import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -127,4 +127,4 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
 ]
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
