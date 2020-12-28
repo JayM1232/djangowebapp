6c1981a4d8976212b71068ce4a3eb4d7519b50aa
tanajim <tanajim580@gmail.com>
2020-12-28 11:41:29 +0530

    changes done

--- linkcre/settings.py
+++ linkcre/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 # from django.conf.global_settings import STATIC_ROOT,STATICFILES_STORAGE,STATICFILES_DIRS,STATIC_URL
-# import django_heroku
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
